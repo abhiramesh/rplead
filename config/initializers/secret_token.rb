9e9ae64bf41203e66361b273b9c64f39bcc1b356
Abhi Ramesh <abhiramesh@gmail.com>
2013-10-14 23:06:30 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rplead::Application.config.secret_token = '1eb3abb35bc81fed7ae9ce31ac8c0d8151418eeeb2c4940f0b3e9e6f3198223e9a944ab1599b1e75e51b9dc6fb52b87ca13473c6c921d7c276953ccd5df5c062'
